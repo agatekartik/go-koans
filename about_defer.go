d50686e61a446619ed02e2bba30e2bcb8d83b705
Kartikeya Agate <agatekartik@gmail.com>
2021-10-01 16:42:14 +0530

    replace tabs with spaces

--- about_defer.go
+++ about_defer.go
@@ -1,43 +1,43 @@
 package go_koans
 
 func aboutDefer() {
-	var acc int
+    var acc int
 
-	increment := func(value int) {
-		acc += value
-	}
+    increment := func(value int) {
+        acc += value
+    }
 
-	decrement := func(value int) {
-		acc -= value
-	}
+    decrement := func(value int) {
+        acc -= value
+    }
 
-	panicRecover := func() {
-		if r := recover(); r != nil {
-			increment(1)
-		}
-	}
+    panicRecover := func() {
+        if r := recover(); r != nil {
+            increment(1)
+        }
+    }
 
-	func() {
-		acc = 0
-		defer increment(1)
-	}()
+    func() {
+        acc = 0
+        defer increment(1)
+    }()
 
-	assert(acc == 1) // defer function will be execute after main function body
+    assert(acc == 1) // defer function will be execute after main function body
 
-	func() {
-		acc = 0
-		defer increment(5)
-		defer decrement(3)
-	}()
+    func() {
+        acc = 0
+        defer increment(5)
+        defer decrement(3)
+    }()
 
-	assert(acc == 2) // list of functions also allowed
+    assert(acc == 2) // list of functions also allowed
 
-	func() {
-		defer panicRecover()
-		acc = 0
-		panic("Expected error")
-	}()
+    func() {
+        defer panicRecover()
+        acc = 0
+        panic("Expected error")
+    }()
 
-	assert(acc == 1) // executed even in case of panic
+    assert(acc == 1) // executed even in case of panic
 
 }
--- about_files.go
+++ about_files.go
@@ -1,13 +1,13 @@
 package go_koans
 
 import (
-	"io/ioutil"
-	"strings"
+    "io/ioutil"
+    "strings"
 )
 
 func aboutFiles() {
-	filename := "about_files.go"
-	contents, _ := ioutil.ReadFile(filename)
-	lines := strings.Split(string(contents), "\n")
-	assert(lines[5] == ")") // handling files is too trivial
+    filename := "about_files.go"
+    contents, _ := ioutil.ReadFile(filename)
+    lines := strings.Split(string(contents), "\n")
+    assert(lines[5] == ")") // handling files is too trivial
 }
